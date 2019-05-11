3c7edddf342aef4e9caa017edbba4b91fa179fba
Xavier Nhagumbe <Xavier_Nhagumbe@pmivectorlink.com>
2019-05-11 11:39:45 +0200

    Initializing

---
+++ src/main/resources/db/migration/V01__initial_schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE empresa(
+codigo INTEGER NOT NULL,
+nome VARCHAR(100) NOT NULL,
+nuit VARCHAR(20) NOT NULL,
+telefone VARCHAR(20),
+celular VARCHAR(20),
+fax VARCHAR(20),
+email VARCHAR(100),
+endereco TEXT,
+PRIMARY KEY(codigo)
+);
